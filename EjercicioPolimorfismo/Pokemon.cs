3f8b5a4997d4c47fa592b245d08084d1d37edace
OAppenzeller <o.appenzeller@yahoo.com>
2024-11-04 18:38:54 -0600

    Add project files.

---
+++ EjercicioPolimorfismo/Pokemon.cs
@@ -0,0 +1,15 @@
+﻿namespace EjercicioPolimorfismo
+{
+    class Pokemon
+    {
+        public virtual void Ataque()
+        {
+            Console.WriteLine("El pokemon usa un ataque");
+        }
+
+        public virtual void Defensa()
+        {
+            Console.WriteLine("El pokemon usa una defensa");
+        }
+    }
+}
